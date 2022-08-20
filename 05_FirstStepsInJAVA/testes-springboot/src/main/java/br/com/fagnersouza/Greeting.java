c09ec8edb19bd3943bda77d44e40add6dc923ea4
Fagner Souza <fagnersouza@gmail.com>
2022-08-20 19:45:28 -0300

    init

---
+++ 05_FirstStepsInJAVA/testes-springboot/src/main/java/br/com/fagnersouza/Greeting.java
@@ -0,0 +1,26 @@
+package br.com.fagnersouza;
+
+public class Greeting {
+
+	private long id;
+	private String content;
+	
+	public Greeting(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+	
+	
+	
+}
